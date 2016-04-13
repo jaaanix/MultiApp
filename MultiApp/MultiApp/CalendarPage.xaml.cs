2c239b16f8fd7b2ba88c48f62d7ef0d71bc17ea2
Giani <Giani@DESKTOP-60KE3TU>
2016-04-13 12:30:01 +0200

    Projektdateien hinzufügen.

---
+++ MultiApp/MultiApp/CalendarPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using MultiApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MultiApp
+{
+    public partial class CalendarPage : ContentPage
+    {
+        public CalendarPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerClick);
+        }
+
+        private bool OnTimerClick()
+        {
+            DateTime dateTime = DateTime.Now;
+            timeLabel.Text = dateTime.ToString("T");
+            dateLabel.Text = dateTime.ToString("D");
+            return true;
+        }
+
+        public void calendarBtnClicked(object s, EventArgs e)
+        {
+            /*
+            var fileName = "database.sqlite";
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, fileName);
+            using (var conn = new SQLite.SQLiteConnection(path))
+            {
+                conn.CreateTable<Events>();
+                var events = new Events { Title = "Anlassen", Date = "10.04.2016" };
+                conn.Insert(events);
+            }
+            */
+        }
+    }
+}
---
+++ MultiApp/MultiApp/UserPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MultiApp.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+using Xamarin.Forms;
+
+namespace MultiApp
+{
+    public partial class UserPage : ContentPage
+    {
+        public UserPage()
+        {
+            InitializeComponent();
+            getUser();
+        }
+
+        public async void getUser()
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("http://jsonplaceholder.typicode.com/users/2");
+            if (result != null)
+            {
+                userName.Text = JsonConvert.DeserializeObject<User>(result).name;
+                userAcc.Text = JsonConvert.DeserializeObject<User>(result).username;
+                userMail.Text = JsonConvert.DeserializeObject<User>(result).email;
+            }
+        }
+    }
+}
