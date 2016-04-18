10e6db6220477711eb089cf6beef89a4599ec3d6
jaaanix <janis.saritzoglou@opitz-consulting.de>
2016-04-18 14:04:57 +0200

    implementiert PullToRefresh für die Liste der Tasks (RefreshTasks-Methode wird nicht ausgeführt)

--- MultiApp/MultiApp/UserPage.xaml.cs
+++ MultiApp/MultiApp/UserPage.xaml.cs
@@ -1,17 +1,39 @@
 ﻿using MultiApp.Models;
 using Newtonsoft.Json;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net.Http;
 
 using Xamarin.Forms;
-using System;
 using System.Collections.Generic;
 
 namespace MultiApp
 {
     public partial class UserPage : ContentPage
     {
+        private bool isRefreshing;
+        public bool IsRefreshing
+        {
+            get { return isRefreshing; }
+            set
+            {
+                if (isRefreshing == value)
+                    return;
+                isRefreshing = value;
+                OnPropertyChanged("IsRefreshing");
+            }
+        }
+        private Command refreshTasks;
+        public Command RefreshTasks
+        {
+            get
+            {
+                return refreshTasks ?? (refreshTasks = new Command(getTasks, () =>
+                {
+                    return !IsRefreshing;
+                }));
+            }
+        }
+
         public UserPage()
         {
             InitializeComponent();
@@ -21,8 +43,12 @@ namespace MultiApp
 
         private async void getTasks()
         {
+            if (IsRefreshing)
+                return;
+            IsRefreshing = true;
+            RefreshTasks.ChangeCanExecute();
             List<Task> liste = new List<Task>();
-            
+
             var client = new HttpClient();
             int todoId = 1;
             while (todoId <= 10) // 10 ToDos fetchen
@@ -33,12 +59,15 @@ namespace MultiApp
                 {
                     // Füllt Liste mit Items (ListView-Einträgen)
                     liste.Add(JsonConvert.DeserializeObject<Task>(result));
-                    
+
                 }
             }
             // Die ListView an die Liste Datenliste mit TODOs binden und dem DataTemplate
             tasksListView.ItemTemplate = dataTemplate;
             tasksListView.ItemsSource = liste;
+
+            IsRefreshing = false;
+            RefreshTasks.ChangeCanExecute();
         }
 
         public async void getUser()
@@ -54,11 +83,30 @@ namespace MultiApp
             }
         }
 
+        private async void updateTaskStatus(Task task)
+        {
+            if (task.Completed.Equals("false"))
+            {
+                HttpContent content = new StringContent("true");
+                var client = new HttpClient();
+                await client.PutAsync("http://jsonplaceholder.typicode.com/todos/" + task.Id, content);
+            }
+            else
+            {
+                HttpContent content = new StringContent("false");
+                var client = new HttpClient();
+                await client.PutAsync("http://jsonplaceholder.typicode.com/todos/" + task.Id, content);
+            }
+        }
+
         void OnItemTapped(object sender, ItemTappedEventArgs e)
         {
             if (e == null) return; // has been set to null, do not 'process' tapped event
             Debug.WriteLine("Tapped: " + e.Item);
+            Debug.WriteLine("Task Id: " + ((Task)e.Item).Id);
+            updateTaskStatus((Task)e.Item);
             ((ListView)sender).SelectedItem = null; // de-select the row
         }
+
     }
 }
